6ee852afbea9b8ac14388f88e37eee9054aff202
msifatsadiq <msifatsadiq@gmail.com>
2024-03-13 00:21:56 +0600

    adding the bottles from api and showing it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Bottles from './components/Bottles/Bottles'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     
-      <h1>Vite + React</h1>
+      <Header></Header>
+      <Bottles></Bottles>
       
     </>
   )
